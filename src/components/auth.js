e6321b0f22c3f1464178a16e21763e97fcc14547
Velius-Gefion <veliusgefion@gmail.com>
2024-01-10 23:35:42 +0800

    First Commit

---
+++ src/components/auth.js
@@ -0,0 +1,27 @@
+
+export const Login = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export const Register = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export const Logout = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAA1TEIaHMFJ3mtGTmElbnJXkDdcyXFM4o",
+  authDomain: "badat-health-tracking.firebaseapp.com",
+  projectId: "badat-health-tracking",
+  storageBucket: "badat-health-tracking.appspot.com",
+  messagingSenderId: "889585687218",
+  appId: "1:889585687218:web:a1419205cc5eee009f07ab"
+};
+
+const app = initializeApp(firebaseConfig);
